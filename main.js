309352e43b09f31847095688ba386cab52c03696
omar.donnadieu <omar.donnadieu@r3continuum.com>
2021-01-19 16:14:32 -0500

    Done

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: "./assets/images/socks_green.jpg",
+            url: "https://www.sockbroker.com/index.php?_route_=men%27s-socks/mens-green-socks/kelly-green-dress-socks-ff323"
         }
     }
 })
